1e7d009db2ba8017870142d5770fa3e188951491
Long Zhao Bi <chuan_long@outlook.com>
2017-09-21 17:42:30 +0800

    modiry

--- hello/src/test/java/com/spring/hello/HelloApplicationTests.java
+++ hello/src/test/java/com/spring/hello/HelloApplicationTests.java
@@ -1,37 +1,37 @@
-package com.spring.hello;
-
-import com.spring.hello.web.HelloController;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = HelloApplication.class)
-public class HelloApplicationTests {
-
-	private MockMvc mvc;
-
-	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-	}
-
-	@Test
-	public void hello() throws Exception{
-		MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andReturn();
-
-		int status = result.getResponse().getStatus();
-		String content = result.getResponse().getContentAsString();
-
-		System.out.println("返回状态码：" + status);
-		System.out.println("返回内容：" + content);
-	}
-
-}
+//package com.spring.hello;
+//
+//import com.spring.hello.web.HelloController;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = HelloApplication.class)
+//public class HelloApplicationTests {
+//
+//	private MockMvc mvc;
+//
+//	@Before
+//	public void setUp() throws Exception {
+//		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+//	}
+//
+//	@Test
+//	public void hello() throws Exception{
+//		MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andReturn();
+//
+//		int status = result.getResponse().getStatus();
+//		String content = result.getResponse().getContentAsString();
+//
+//		System.out.println("返回状态码：" + status);
+//		System.out.println("返回内容：" + content);
+//	}
+//
+//}
